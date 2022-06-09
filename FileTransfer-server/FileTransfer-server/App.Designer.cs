7c2953121bdc17b0190b0d4b8b5d04f1e0dc9bd3
tomeckiStudio <85648788+tomeckiStudio@users.noreply.github.com>
2022-06-09 10:35:54 +0200

    Add files via upload

---
+++ FileTransfer-server/FileTransfer-server/App.Designer.cs
@@ -0,0 +1,128 @@
+﻿namespace FileTransfer_server
+{
+    partial class App
+    {
+        /// <summary>
+        /// Wymagana zmienna projektanta.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Wyczyść wszystkie używane zasoby.
+        /// </summary>
+        /// <param name="disposing">prawda, jeżeli zarządzane zasoby powinny zostać zlikwidowane; Fałsz w przeciwnym wypadku.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Kod generowany przez Projektanta formularzy systemu Windows
+
+        /// <summary>
+        /// Metoda wymagana do obsługi projektanta — nie należy modyfikować
+        /// jej zawartości w edytorze kodu.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(App));
+            this.pb_status = new System.Windows.Forms.ProgressBar();
+            this.tb_path = new System.Windows.Forms.TextBox();
+            this.folderBrowserDialog = new System.Windows.Forms.FolderBrowserDialog();
+            this.btn_browse_folder = new System.Windows.Forms.Button();
+            this.btn_start_Server = new System.Windows.Forms.Button();
+            this.debug_window = new System.Windows.Forms.Label();
+            this.panel_debug_window = new System.Windows.Forms.Panel();
+            this.panel_debug_window.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // pb_status
+            // 
+            this.pb_status.Location = new System.Drawing.Point(15, 165);
+            this.pb_status.Name = "pb_status";
+            this.pb_status.Size = new System.Drawing.Size(451, 23);
+            this.pb_status.TabIndex = 0;
+            // 
+            // tb_path
+            // 
+            this.tb_path.Location = new System.Drawing.Point(12, 75);
+            this.tb_path.Name = "tb_path";
+            this.tb_path.Size = new System.Drawing.Size(451, 20);
+            this.tb_path.TabIndex = 3;
+            this.tb_path.Text = "D:/";
+            this.tb_path.TextChanged += new System.EventHandler(this.Path_changed);
+            // 
+            // btn_browse_folder
+            // 
+            this.btn_browse_folder.Location = new System.Drawing.Point(15, 101);
+            this.btn_browse_folder.Name = "btn_browse_folder";
+            this.btn_browse_folder.Size = new System.Drawing.Size(106, 23);
+            this.btn_browse_folder.TabIndex = 4;
+            this.btn_browse_folder.Text = "Browse folder";
+            this.btn_browse_folder.UseVisualStyleBackColor = true;
+            this.btn_browse_folder.Click += new System.EventHandler(this.btn_browse_folder_Click);
+            // 
+            // btn_start_Server
+            // 
+            this.btn_start_Server.Location = new System.Drawing.Point(141, 12);
+            this.btn_start_Server.Name = "btn_start_Server";
+            this.btn_start_Server.Size = new System.Drawing.Size(172, 23);
+            this.btn_start_Server.TabIndex = 5;
+            this.btn_start_Server.Text = "Start server";
+            this.btn_start_Server.UseVisualStyleBackColor = true;
+            this.btn_start_Server.Click += new System.EventHandler(this.btn_start_Server_Click);
+            // 
+            // debug_window
+            // 
+            this.debug_window.AutoSize = true;
+            this.debug_window.Location = new System.Drawing.Point(-3, 0);
+            this.debug_window.MinimumSize = new System.Drawing.Size(454, 223);
+            this.debug_window.Name = "debug_window";
+            this.debug_window.Size = new System.Drawing.Size(454, 223);
+            this.debug_window.TabIndex = 7;
+            // 
+            // panel_debug_window
+            // 
+            this.panel_debug_window.AutoScroll = true;
+            this.panel_debug_window.Controls.Add(this.debug_window);
+            this.panel_debug_window.Location = new System.Drawing.Point(15, 215);
+            this.panel_debug_window.Name = "panel_debug_window";
+            this.panel_debug_window.Size = new System.Drawing.Size(451, 223);
+            this.panel_debug_window.TabIndex = 8;
+            // 
+            // App
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(478, 450);
+            this.Controls.Add(this.panel_debug_window);
+            this.Controls.Add(this.btn_start_Server);
+            this.Controls.Add(this.btn_browse_folder);
+            this.Controls.Add(this.tb_path);
+            this.Controls.Add(this.pb_status);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "App";
+            this.Text = "File Transfer";
+            this.Load += new System.EventHandler(this.App_Load);
+            this.panel_debug_window.ResumeLayout(false);
+            this.panel_debug_window.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ProgressBar pb_status;
+        private System.Windows.Forms.TextBox tb_path;
+        private System.Windows.Forms.FolderBrowserDialog folderBrowserDialog;
+        private System.Windows.Forms.Button btn_browse_folder;
+        private System.Windows.Forms.Button btn_start_Server;
+        private System.Windows.Forms.Label debug_window;
+        private System.Windows.Forms.Panel panel_debug_window;
+    }
+}
+
---
+++ FileTransfer-server/FileTransfer-server/App.cs
@@ -0,0 +1,253 @@
+﻿using System;
+using System.Diagnostics;
+using System.Text;
+using System.Threading;
+using System.Windows.Forms;
+using java.io;
+using java.lang;
+using java.net;
+
+namespace FileTransfer_server
+{
+    public partial class App : Form
+    {
+        private ServerSocket server_socket;
+        private Socket target_socket;
+        private DataInputStream din;
+        private DataOutputStream dout;
+        System.Threading.Thread thread_receive_file = null;
+        private RandomAccessFile rw = null;
+
+        private string path = "D:/";
+        private string file_name = "";
+        private bool isServerRunning = false;
+        private bool stopBackgroundService = false;
+
+        public App()
+        {
+            InitializeComponent();
+        }
+
+        private void App_Load(object sender, EventArgs e)
+        {
+            
+        }
+
+        public void StartServer()
+        {
+            try
+            {
+                debug_window.Text = "Starting server..." + "\n" + debug_window.Text;
+                server_socket = new ServerSocket(6868);
+                debug_window.Text = "Waiting for file..." + "\n" + debug_window.Text;
+                
+                thread_receive_file = new System.Threading.Thread(new ThreadStart(ReceiveFile));
+                thread_receive_file.Start();
+            }
+            catch (System.Exception ex)
+            {
+                Debug.WriteLine("StartServer: " + ex.ToString());
+            }
+        }
+
+        private void StopServer()
+        {
+            stopBackgroundService = true;
+
+            if(dout != null)
+                dout.close();
+
+            if(target_socket != null)
+                target_socket.close();
+
+            if(server_socket != null)
+                server_socket.close();
+
+            if(din != null)
+                din.close();
+
+            if(rw != null)
+                rw.close();
+
+            debug_window.Invoke(new Action(delegate ()
+            {
+                debug_window.Text = "========= Server stopped =========" + "\n" + debug_window.Text;
+            }));
+
+            pb_status.Invoke(new Action(delegate ()
+            {
+                pb_status.Value = 0;
+            }));
+        }
+
+        public void ReceiveFile()
+        {
+            try
+            {
+                target_socket = server_socket.accept();
+                din = new DataInputStream(target_socket.getInputStream());
+                dout = new DataOutputStream(target_socket.getOutputStream());
+            
+                long current_file_pointer = 0;
+                while (!stopBackgroundService)
+                {
+                    byte[] initilize = new byte[1];
+                
+                    din.read(initilize, 0, initilize.Length);
+
+                    if (initilize[0] == 2)
+                    {
+                        byte[] cmd_buff = new byte[3];
+                        din.read(cmd_buff, 0, cmd_buff.Length);
+                        byte[] recv_data = ReadStream();
+
+                        switch (Integer.parseInt(Encoding.UTF8.GetString(cmd_buff)))
+                        {
+                            case 124:
+                                file_name = Encoding.UTF8.GetString(recv_data);
+                                rw = new RandomAccessFile(path + file_name, "rw");
+                                dout.write(CreateDataPacket(Encoding.UTF8.GetBytes("125"), Encoding.UTF8.GetBytes(java.lang.String.valueOf(current_file_pointer))));
+                                dout.flush();
+                                debug_window.Invoke(new Action(delegate ()
+                                {
+                                    debug_window.Text = "Receiving file: " + file_name + "..." + "\n" + debug_window.Text;
+                                }));
+                                break;
+                            case 126:
+                                rw.seek(current_file_pointer);
+                                rw.write(recv_data);
+                                current_file_pointer = rw.getFilePointer();
+                                pb_status.Invoke(new Action(delegate ()
+                                {
+                                    pb_status.Value = Convert.ToInt32(((float)current_file_pointer / rw.length()) * 100);
+                                }));
+
+                                dout.write(CreateDataPacket(Encoding.UTF8.GetBytes("125"), Encoding.UTF8.GetBytes(java.lang.String.valueOf(current_file_pointer))));
+                                dout.flush();
+                                break;
+                            case 127:
+                                if ("Close".Equals(Encoding.UTF8.GetString(recv_data)))
+                                {
+                                    dout.close();
+                                    target_socket.close();
+                                    server_socket.close();
+                                    din.close();
+                                    rw.close();
+                                    current_file_pointer = 0;
+
+                                    debug_window.Invoke(new Action(delegate ()
+                                    {
+                                        debug_window.Text = "File received and saved in: " + path + file_name + "\n" + debug_window.Text;
+                                    }));
+
+                                    pb_status.Invoke(new Action(delegate ()
+                                    {
+                                        pb_status.Value = 0;
+                                    }));
+
+                                    server_socket = new ServerSocket(6868);
+                                    target_socket = server_socket.accept();
+                                    din = new DataInputStream(target_socket.getInputStream());
+                                    dout = new DataOutputStream(target_socket.getOutputStream());
+                                }
+                                break;
+                        }
+                    }
+                }
+            }
+            catch (System.Exception ex)
+            {
+                Debug.WriteLine("ReceiveFile: " + ex.ToString());
+            }
+        }
+
+        private byte[] ReadStream()
+        {
+            byte[] data_buff = null;
+            try
+            {
+                int b = 0;
+                System.String buff_length = "";
+                while ((b = din.read()) != 4)
+                {
+                    buff_length += (char)b;
+                }
+                int data_length = Integer.parseInt(buff_length);
+                data_buff = new byte[Integer.parseInt(buff_length)];
+                int byte_read = 0;
+                int byte_offset = 0;
+                while (byte_offset < data_length)
+                {
+                    byte_read = din.read(data_buff, byte_offset, data_length - byte_offset);
+                    byte_offset += byte_read;
+                }
+            }
+            catch (System.Exception ex)
+            {
+                Debug.WriteLine("ReadStream: " + ex.ToString());
+                debug_window.Invoke(new Action(delegate ()
+                {
+                    debug_window.Text = "ReadStream: " + ex.ToString() + "\n" + debug_window.Text;
+                }));
+            }
+            return data_buff;
+        }
+
+        private byte[] CreateDataPacket(byte[] cmd, byte[] data)
+        {
+            byte[] packet = null;
+            try
+            {
+                byte[] initialize = new byte[1];
+                initialize[0] = 2;
+                byte[] separator = new byte[1];
+                separator[0] = 4;
+
+                byte[] data_length = Encoding.UTF8.GetBytes(java.lang.String.valueOf(data.Length));
+                packet = new byte[initialize.Length + cmd.Length + separator.Length + data_length.Length + data.Length];
+
+                Array.Copy(initialize, 0, packet, 0, initialize.Length);
+                Array.Copy(cmd, 0, packet, initialize.Length, cmd.Length);
+                Array.Copy(data_length, 0, packet, initialize.Length + cmd.Length, data_length.Length);
+                Array.Copy(separator, 0, packet, initialize.Length + cmd.Length + data_length.Length, separator.Length);
+                Array.Copy(data, 0, packet, initialize.Length + cmd.Length + data_length.Length + separator.Length, data.Length);
+
+            }
+            catch (System.Exception ex)
+            {
+                Debug.WriteLine("CreateDataPacket: " + ex.ToString());
+            }
+            return packet;
+        }
+
+        private void Path_changed(object sender, EventArgs e)
+        {
+            path = tb_path.Text;
+        }
+
+        private void btn_browse_folder_Click(object sender, EventArgs e)
+        {
+            if (folderBrowserDialog.ShowDialog() == DialogResult.OK)
+            {
+                tb_path.Text = folderBrowserDialog.SelectedPath;
+            }
+        }
+
+        private void btn_start_Server_Click(object sender, EventArgs e)
+        {
+            if (isServerRunning)
+            {
+                isServerRunning = false;
+                btn_start_Server.Text = "Start server";
+                StopServer();
+            }
+            else
+            {
+                isServerRunning = true;
+                btn_start_Server.Text = "Stop server";
+                StartServer();
+            }
+        }
+
+    }
+}
